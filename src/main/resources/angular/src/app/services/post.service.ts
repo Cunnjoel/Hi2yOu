b04a854edfe740e983a3508043eb3d58b5a2ca87
jyost13088 <jyost13088@comcast.net>
2022-04-15 16:13:48 -0400

    merged services

--- src/main/resources/angular/src/app/services/post.service.ts
+++ src/main/resources/angular/src/app/services/post.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private httpCli : HttpClient) { }
+
+  createPost(message : string, picture : string, users : Array<object>, date : string, user : object ){
+      return this.httpCli.post<any>(`http://localhost:9000/post`, { ""}
+      , {
+        withCredentials: true
+      });
+    }
+
+  getPostByUserId(userId : number){
+      return this.httpCli.get<any>(`http://localhost:9000/post/${userId}``, {
+         "postId": postId.
+         "message": message,
+         "picture": picture,
+         "users": users,
+         "user": user
+      }
+      , {
+        withCredentials: true
+      });
+    }
+
+     getPostByUserId(userId : number){
+         return this.httpCli.get<any>(`http://localhost:9000/post`, {
+            Array<object>
+         }
+         , {
+           withCredentials: true
+         });
+       }
+
 }
