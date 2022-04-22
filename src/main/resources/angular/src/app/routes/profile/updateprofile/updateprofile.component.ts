1679a94f3146ef99cb0dbb6d66f6f0871c2852fb
Zachary Starkey <zstarkey@icloud.com>
2022-04-22 03:15:57 -0400

    trying to set up editing of user

--- src/main/resources/angular/src/app/routes/profile/updateprofile/updateprofile.component.ts
+++ src/main/resources/angular/src/app/routes/profile/updateprofile/updateprofile.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Profile } from 'src/app/models/Profile';
 import { User } from 'src/app/models/User';
 import { ProfileService } from 'src/app/services/profile.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-updateprofile',
@@ -15,8 +16,9 @@ export class UpdateprofileComponent implements OnInit {
   fileUrl : any;
   profile : Profile = <Profile>{};
   picture : boolean = false;
+  errorM : string = "";
 
-  constructor(private profileService : ProfileService, private router : Router) { }
+  constructor(private profileService : ProfileService, private router : Router,private userService : UserService) { }
 
   ngOnInit(): void {
     this.profileService.getProfileById(this.profileService.getIdNumberFromUrl(this.router.url,15)).subscribe(responseBody=>{
@@ -46,10 +48,21 @@ export class UpdateprofileComponent implements OnInit {
         {
           this.profile.pictureUrl = "https://" + reponseBody.fileUrl;
         }
-        this.profileService.updateProfile(this.profile).subscribe(responseBody=>{
-            this.profileService.currentUserProfile = responseBody;
-            this.router.navigate(['/viewprofile/user/' + this.profileService.currentUserProfile.id])
-        });
+        this.userService.update(this.profile.user).subscribe(responseBody=>
+          {
+            this.profile.user = responseBody;
+            if (this.profile.user.userId == null)
+            {
+              this.errorM = "Username already taken"
+            }
+            else
+            {
+              this.profileService.updateProfile(this.profile).subscribe(responseBody=>{
+                this.profileService.currentUserProfile = responseBody;
+                this.router.navigate(['/viewprofile/user/' + this.profileService.currentUserProfile.id])
+            });
+            }
+          })
       });
   }
 }
